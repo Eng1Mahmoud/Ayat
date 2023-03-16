f1765201e73002d4b0cbab62f31b262ac41d7209
Eng1Mahmoud <mahmoudmohamedabdelaal1@gmail.com>
2023-03-16 10:02:56 +0200

    uoload project

---
+++ frontend_Ayat/src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ErrorPage = () => {
+  return (
+    <div className="py-5 errorPage">
+      <div className="py-5 my-5">
+        <h2 className="py-5 my-5 text-center">عذرا هذا الصفحة غير موجودة</h2>
+      </div>
+    </div>
+  );
+};
---
+++ frontend_Ayat/src/router.js
@@ -0,0 +1,80 @@
+import App from "./App";
+import { Home } from "./pages/Home";
+import { SignUpPage } from "./pages/SignUpPage";
+import { VerifyCodePage } from "./pages/VerifyCodePage";
+import { ContactPage } from "./pages/ContactPage";
+import { Admin } from "./pages/Admin";
+import SignIn from "./components/admin/Login"
+import { Dashboard } from "./components/admin/Dashboard";
+import AddMessage from "./components/admin/AddMessage";
+import Replay  from "./components/admin/Replay";
+import { Users } from "./components/admin/Users";
+import { Mange } from "./components/admin/Mange";
+import {ErrorPage} from "./pages/ErrorPage"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export const  router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "signUp",
+          element: <SignUpPage />,
+        },
+        {
+          path: "VerifyCode",
+          element: <VerifyCodePage />,
+        },
+        {
+          path: "contact us",
+          element: <ContactPage />,
+        },
+        {
+          path: "*",
+          element: <ErrorPage />,
+        },
+        {
+          path: "admin",
+          element: <Admin />,
+          children: [
+            {
+              index: true,
+      
+              element: <SignIn />,
+            },
+            {
+           
+              path: "dashboard",
+              element:<Dashboard/>,
+              children: [
+                {
+                 index:true,
+                  element: <Mange />,
+                },
+                {
+                  path: "replay",
+                  element: <Replay />,
+                },
+                {
+                  path: "addMessage",
+                  element: <AddMessage />,
+                },
+                {
+                  path: "users",
+                  element: <Users />,
+                },
+               
+              ]
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  
